aa72dafc80ede01943df98527e99c40d51b0a48a
IlayCarvalho <suedilay0404@gmail.com>
2024-07-20 20:09:34 -0300

    Add página de formulário

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MomentFormComponent } from "../../moment-form/moment-form.component";
 
 @Component({
   selector: 'app-new-moment',
   standalone: true,
-  imports: [],
+  imports: [
+    MomentFormComponent
+  ],
   templateUrl: './new-moment.component.html',
   styleUrl: './new-moment.component.css'
 })
 export class NewMomentComponent {
-
+  btnText = 'Compartilhar';
 }
