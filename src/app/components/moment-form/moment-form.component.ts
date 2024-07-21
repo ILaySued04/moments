355f50b72b13a1f9aea16fb4691c372838eae012
IlayCarvalho <suedilay0404@gmail.com>
2024-07-21 12:13:17 -0300

    Validando formulário com Reactive Forms

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,12 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject, Input, OnInit} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf,
+  ],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
 export class MomentFormComponent {
   @Input() btnText!: string;
+
+  private formBuilder = inject(FormBuilder)
+
+  protected momentForm = this.formBuilder.group({
+    id: [''],
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    image: [''],
+  })
+
+  async submit() {
+
+    if (this.momentForm.invalid) {
+      return ;
+    }
+
+    console.log("enviou")
+  }
 }
